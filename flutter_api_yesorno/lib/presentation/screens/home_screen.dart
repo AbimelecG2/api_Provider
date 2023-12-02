2e54f9ffe819162bc4a902e749d26ae8efbebba9
Abimelec <abimelecg2@unitec.edu>
2023-12-02 10:59:09 -0600

    pregunta rrespuesta boton

--- flutter_api_yesorno/lib/presentation/screens/home_screen.dart
+++ flutter_api_yesorno/lib/presentation/screens/home_screen.dart
@@ -2,19 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_api_yesorno/providers/respuesta_provider.dart';
 import 'package:provider/provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController questionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final respuestaProvider = context.watch<RespuestaProvider>();
-    respuestaProvider.loadRespuesta();
+
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(respuestaProvider.mensaje.answer),
-        Image.network(respuestaProvider.mensaje.image)
-      ],
-    ));
+      appBar: AppBar(title: Text('Haz una Pregunta')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: questionController,
+              decoration: InputDecoration(
+                labelText: 'Ingresa tu pregunta',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => respuestaProvider.loadRespuesta(),
+              child: Text('Obtener Respuesta'),
+            ),
+            SizedBox(height: 20),
+            if (respuestaProvider.mensaje.answer.isNotEmpty)
+              Text(respuestaProvider.mensaje.answer),
+            if (respuestaProvider.mensaje.image.isNotEmpty)
+              Image.network(respuestaProvider.mensaje.image),
+          ],
+        ),
+      ),
+    );
   }
 }
